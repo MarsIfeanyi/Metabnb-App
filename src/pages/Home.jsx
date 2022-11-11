41f006d9c4d063ff0f3b206303f8d0c373fd7252
MarsIfeanyi <marschalice@gmail.com>
2022-11-11 11:28:46 +0100

    Update Home Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import CTASect from "../components/CTASect";
 import HeroSect from "../components/HeroSect";
+import MainSect from "../components/MainSect";
 
 const Home = () => {
   return (
     <div>
       <HeroSect />
+
+      <MainSect />
+
+      <CTASect />
     </div>
   );
 };
