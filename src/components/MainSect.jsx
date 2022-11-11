dbba41eaaf2afa05600a92a1850a09aab73a16c6
MarsIfeanyi <marschalice@gmail.com>
2022-11-11 11:22:34 +0100

    Create and render Main Section component

---
+++ src/components/MainSect.jsx
@@ -0,0 +1,136 @@
+import React from "react";
+
+const MainSect = () => {
+  return (
+    <div className="container mt-14 items-center mx-auto justify-center py-4 px-8 max-w-7xl">
+      <h2 className="h2Style pt-2">Inspiration for your next adventure</h2>
+      <div className="gridContainer ">
+        {/* 01 */}
+        <div className="imgContStyle">
+          <img src="/desertKing01.png" alt="desertKing01-Image" />
+          <div className="flex flex-col ">
+            <div className="contentStyle01">
+              <h5>Desert king</h5>
+              <h4 className="font-bold">1MBT per night</h4>
+            </div>
+            <div className="contentStyle02">
+              <p>2345km away</p>
+              <p>available for 2weeks stay</p>
+            </div>
+          </div>
+
+          <img src="/starRating.png" alt="starRating-Image" />
+        </div>
+        {/* 02 */}
+        <div className="imgContStyle">
+          <img src="/desertKing02.png" alt="desertKing02-Image" />
+
+          <div className="flex flex-col ">
+            <div className="contentStyle01">
+              <h5>Desert king</h5>
+              <h4 className="font-bold">1MBT per night</h4>
+            </div>
+            <div className="contentStyle02">
+              <p>2345km away</p>
+              <p>available for 2weeks stay</p>
+            </div>
+          </div>
+          <img src="/starRating.png" alt="starRating-Image" />
+        </div>
+        {/* 03 */}
+        <div className="imgContStyle">
+          <img src="/desertKing03.png" alt="desertKing03-Image" />
+          <div className="flex flex-col ">
+            <div className="contentStyle01">
+              <h5>Desert king</h5>
+              <h4 className="font-bold">1MBT per night</h4>
+            </div>
+            <div className="contentStyle02">
+              <p>2345km away</p>
+              <p>available for 2weeks stay</p>
+            </div>
+          </div>
+          <img src="/starRating.png" alt="starRating-Image" />
+        </div>
+        {/* 04 */}
+        <div className="imgContStyle">
+          <img src="/desertKing04.png" alt="desertKing04-Image" />
+          <div className="flex flex-col ">
+            <div className="contentStyle01">
+              <h5>Desert king</h5>
+              <h4 className="font-bold">1MBT per night</h4>
+            </div>
+            <div className="contentStyle02">
+              <p>2345km away</p>
+              <p>available for 2weeks stay</p>
+            </div>
+          </div>
+          <img src="/starRating.png" alt="starRating-Image" />
+        </div>
+
+        {/* 05 */}
+        <div className="imgContStyle">
+          <img src="/desertKing05.png" alt="desertKing05-Image" />
+          <div className="flex flex-col ">
+            <div className="contentStyle01">
+              <h5>Desert king</h5>
+              <h4 className="font-bold">1MBT per night</h4>
+            </div>
+            <div className="contentStyle02">
+              <p>2345km away</p>
+              <p>available for 2weeks stay</p>
+            </div>
+          </div>
+          <img src="/starRating.png" alt="starRating-Image" />
+        </div>
+        {/* 06 */}
+        <div className="imgContStyle">
+          <img src="/desertKing06.png" alt="desertKing06-Image" />
+          <div className="flex flex-col ">
+            <div className="contentStyle01">
+              <h5>Desert king</h5>
+              <h4 className="font-bold">1MBT per night</h4>
+            </div>
+            <div className="contentStyle02">
+              <p>2345km away</p>
+              <p>available for 2weeks stay</p>
+            </div>
+          </div>
+          <img src="/starRating.png" alt="starRating-Image" />
+        </div>
+        {/* 07 */}
+        <div className="imgContStyle">
+          <img src="/desertKing07.png" alt="desertKing07-Image" />
+          <div className="flex flex-col ">
+            <div className="contentStyle01">
+              <h5>Desert king</h5>
+              <h4 className="font-bold">1MBT per night</h4>
+            </div>
+            <div className="contentStyle02">
+              <p>2345km away</p>
+              <p>available for 2weeks stay</p>
+            </div>
+          </div>
+          <img src="/starRating.png" alt="starRating-Image" />
+        </div>
+        {/* 08 */}
+        <div className="imgContStyle">
+          <img src="/desertKing08.png" alt="desertKing08-Image" />
+          <div className="flex flex-col ">
+            <div className="contentStyle01">
+              <h5>Desert king</h5>
+              <h4 className="font-bold">1MBT per night</h4>
+            </div>
+            <div className="contentStyle02">
+              <p>2345km away</p>
+              <p>available for 2weeks stay</p>
+            </div>
+          </div>
+          <img src="/starRating.png" alt="starRating-Image" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MainSect;
