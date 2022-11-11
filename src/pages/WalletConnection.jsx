386f09f2a5e9ad9b47e71ebce74dd5d049d5851d
MarsIfeanyi <marschalice@gmail.com>
2022-11-11 21:39:38 +0100

    Update page

--- src/pages/WalletConnection.jsx
+++ src/pages/WalletConnection.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const WalletConnection = () => {
-  return <div className="text-5xl">WalletConnection</div>;
+  return <div className="text-5xl text-center">WalletConnection</div>;
 };
 
 export default WalletConnection;
