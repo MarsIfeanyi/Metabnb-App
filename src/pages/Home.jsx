b8eb67b44b51c19f27d52c9ca36a97ec7ad85eb4
MarsIfeanyi <marschalice@gmail.com>
2022-11-11 06:39:41 +0100

    Initial Commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HeroSect from "../components/HeroSect";
+
+const Home = () => {
+  return (
+    <div>
+      <HeroSect />
+    </div>
+  );
+};
+
+export default Home;
