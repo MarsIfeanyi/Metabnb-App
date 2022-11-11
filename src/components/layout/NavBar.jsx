196f8ec8eb783a1b04aaf8090674b747a16c2930
MarsIfeanyi <marschalice@gmail.com>
2022-11-11 11:26:40 +0100

    Update Navbar Component

--- src/components/layout/NavBar.jsx
+++ src/components/layout/NavBar.jsx
@@ -2,19 +2,18 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { logo } from "../../assets";
 import ConnectWallet from "../ConnectWallet";
-import HeroSect from "../HeroSect";
 
 const NavBar = (props) => {
   return (
     <>
-      <div className="flex flex-col md:flex-row mt-10 md:mt-6 items-center justify-between space-y-10 p-2">
+      <div className="flex flex-col md:flex-row mt-10 md:mt-6 items-center mx-auto justify-between space-y-10 py-2 px-8 max-w-7xl">
         <header>
           <img src={logo} alt="logo-Image" />
         </header>
 
         <nav>
-          <ul className="flex flex-row space-x-8">
-            <li>
+          <ul className="flex flex-row space-x-8 font-redRose text-xl  ">
+            <li className="hover:border-b-2 border-spanColor">
               <Link to="/">Home</Link>
             </li>
             <li>
