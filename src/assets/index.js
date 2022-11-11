34bb8fee669cb544ff258cd81136ab86651413de
MarsIfeanyi <marschalice@gmail.com>
2022-11-11 11:24:47 +0100

    Add Images

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,9 @@
 import logo from "./logo.png";
 import heroImg from "./heroImg.png";
+import ctaImg from "./ctaImg.png";
+import footerLogo from "./footerLogo.svg";
+import fbIcon from "./fbIcon.png";
+import instIcon from "./instICon.png";
+import twitIcon from "./twitIcon.png";
 
-export { logo, heroImg };
+export { logo, heroImg, ctaImg, footerLogo, fbIcon, instIcon, twitIcon };
