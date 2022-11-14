d56c52febc67816128438d08ead5e192f996b395
MarsIfeanyi <marschalice@gmail.com>
2022-11-15 00:54:16 +0100

    Update File

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,13 @@
 import React from "react";
-import { useState } from "react";
-import ConnectWallet from "../components/ConnectWallet";
 
 import CTASect from "../components/CTASect";
 import HeroSect from "../components/HeroSect";
 import MainSect from "../components/MainSect";
-import WalletModal from "../components/WalletModal";
-import WalletConnection from "./WalletConnection";
 
 const Home = () => {
-  const [openModal, setOpenModal] = useState(false);
   return (
     <div>
       <HeroSect />
-
-      {/* Modal */}
-      <button onClick={() => setOpenModal(true)} className="modalButton">
-        {/* Modal */}
-      </button>
-      <WalletModal open={openModal} onClose={() => setOpenModal(false)} />
 
       <MainSect />
 
