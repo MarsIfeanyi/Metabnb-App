2caff51ae0e93735f0632937e93fbd33e17caeea
MarsIfeanyi <marschalice@gmail.com>
2022-11-15 00:42:36 +0100

    Update file

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,11 +2,19 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
     extend: {
       colors: {
         bgFooter: "#1D1D1E",
         spanColor: "#a02279 ",
         dimBlack: "#434343",
+        dimGray: "#CFD8DC",
+        dimBlack2: "#333333",
       },
 
       fontFamily: {
