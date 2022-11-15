8eebce586d8b9f07de5722907d3c84a8f4db833b
MarsIfeanyi <marschalice@gmail.com>
2022-11-15 02:53:49 +0100

    Update file

--- src/components/layout/NavBar.jsx
+++ src/components/layout/NavBar.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { logo } from "../../assets";
+import { mobileLogo, logo } from "../../assets";
 import ConnectWallet from "../ConnectWallet";
 
 const NavBar = (props) => {
   return (
     <>
-      <div className="flex flex-col md:flex-row mt-10 md:mt-6 items-center mx-auto justify-between space-y-10 py-2 px-8 max-w-7xl shadow-sm">
+      <div className="flex flex-col md:flex-row mt-0 md:mt-6 items-start md:items-center md:mx-auto justify-between space-y-10 py-6 px-12 md:px-8 max-w-7xl shadow-sm bg-spanColor md:bg-white ">
         <header>
-          <img src={logo} alt="logo-Image" />
+          <img src={logo} alt="logo-Image" className="hidden md:flex" />
+          <img
+            src={mobileLogo}
+            alt="mobileScreen-Logo"
+            className="flex md:hidden"
+          />
         </header>
 
         <nav>
@@ -27,7 +32,7 @@ const NavBar = (props) => {
             </li>
           </ul>
 
-          <ul className="grid grid-cols-2 md:hidden gap-2">
+          <ul className="grid grid-cols-2 md:hidden gap-2 text-white text-xl mt-4">
             <li className="hover:border-b-2 border-spanColor">
               <Link to="/">Home</Link>
             </li>
